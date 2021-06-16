f2db868b712096692b112068edb60f8571e72523
Antonio Sebastiano <sebastianoa@hotmail.com>
2021-06-16 14:36:16 +1000

    completed task8 update status

--- JS/taskManager.js
+++ JS/taskManager.js
@@ -1,7 +1,12 @@
-const createTaskHtml = (name, description, assignedTo, dueDate, status)  => {
-
+const createTaskHtml = (id, name, description, assignedTo, dueDate, status)  => {
+  let visibility;
+  if(status === "Done"){
+    visibility = "invisible";
+  }else{
+    visibility = "visible";
+  }
   const html = `
-  <li class="list-group-item list-border">
+  <li class="list-group-item list-border" data-task-id="${id}">
   <div class="card cardstyle">
     <div class="card-body">
       <h3 class="card-title">${name}</h3>
@@ -12,7 +17,7 @@ const createTaskHtml = (name, description, assignedTo, dueDate, status)  => {
         <h6 class="card-subtitle my-3 mt-4">Status: <span class="strong">${status}</span></h6>
         <div class="container">
           <div class="row row-cols-1 row-cols-md-4 justify-content-between ">
-            <button class="col-4 btn btn-outline-success"><i class="fa fa-thumbs-o-up "></i>Done</button> 
+            <button class="col-4 btn btn-outline-success done-button ${visibility}"><i class="fa fa-thumbs-o-up "></i>Done</button> 
             <button class="col-4 btn btn-outline-danger"><i class="fa fa-trash"></i>Delete</button>
           </div>
         </div>
@@ -38,28 +43,40 @@ class TaskManager {
             dueDate: dueDate,
             status: status
         };
-        this.tasks.push({ newTask });
+        this.tasks.push(newTask);
     }
     render(){
         const tasksHtmlList = [];
         for(let i = 0; i < this.tasks.length; i++){
           const renderTask = this.tasks[i];
           //console.log(renderTask);
-          const date = new Date(renderTask.newTask.dueDate);
+          const date = new Date(renderTask.dueDate);
           //console.log(date);
           const formattedDate = ("0" + date.getDate()).slice(-2) + "/" + ("0" + (date.getMonth() + 1)).slice(-2) + "/" + date.getFullYear();
           //console.log(formattedDate);
           const taskHtml = createTaskHtml(
-            renderTask.newTask.name, 
-            renderTask.newTask.description, 
-            renderTask.newTask.assignedTo, 
+            renderTask.id,
+            renderTask.name, 
+            renderTask.description, 
+            renderTask.assignedTo, 
             formattedDate, 
-            renderTask.newTask.status);
+            renderTask.status);
           tasksHtmlList.push(taskHtml);
           //console.log(taskHtml);
         }
         const taskHtml = tasksHtmlList.join("\n");
-        const taskList = document.querySelector("#displayTask");
+        const taskList = document.querySelector("#taskList");
         taskList.innerHTML = taskHtml;
+    }
+
+    getTaskById(taskId){
+      let foundTask;
+      for(let i = 0; i < this.tasks.length; i++){
+        const task = this.tasks[i];
+        if(task.id === taskId){
+          foundTask = task;
+        }
+      }
+      return foundTask;
     }
 };
